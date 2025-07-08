92fc8536ae20014d0de3594ff6afc6b2e4331316
Anunay Aatipamula <anunayaatipamul.dml21@iiitb.net>
2025-07-08 21:03:55 +0530

    update page redirection

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Anunay';
+export const SITE_TITLE = 'Anunay Aatipamula';
 export const SITE_DESCRIPTION = 'Anunay Personal Portfolio';
