5193f289e29a77a021eefd2fb720fd700eb55a73
anunay-aatipamula <anunay.aatipamula@salesforce.com>
2025-07-10 11:33:25 +0530

    Update site configuration to use apex domain anunay.dev

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://www.anunay.dev',
+  site: 'https://anunay.dev',
   integrations: [mdx(), sitemap(), tailwind()]
 });
